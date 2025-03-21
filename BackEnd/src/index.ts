26270629ec3848b7aaff5b727a9dcfd252c281e1
yair-bataglia <batagliayair@gmail.com>
2025-03-21 11:50:16 -0300

    arreglos varios

--- BackEnd/src/index.ts
+++ BackEnd/src/index.ts
@@ -1,7 +1,19 @@
+const cors = require('cors');
 import express from 'express';
 import usersRoute from './routes/usersRoute'
 
 const app = express()
+
+// Configuración de CORS
+const corsOptions = {
+    origin: 'http://localhost:5173',  // Permite solicitudes desde localhost:5173 (puedes agregar más orígenes si es necesario)
+    methods: ['GET', 'POST'],        // Especifica qué métodos HTTP se permiten
+    allowedHeaders: ['Content-Type'] // Especifica los encabezados permitidos
+  };
+  
+  // Habilitar CORS usando las opciones anteriores
+  app.use(cors(corsOptions));
+
 
 const PORT = process.env.PORT || 3000;
 
--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:3000',
+      '/api': 'https://api-challenger-forit.onrender.com',
     }
   }
 });
